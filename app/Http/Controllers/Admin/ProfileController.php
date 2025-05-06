adb7c0a8a6adfb60f2e420dd741a18313431477a
NikoKenara <nikokenara@gmail.com>
2025-05-06 23:06:13 +0700

    Admin - Admin Avatar Update Feature (Part - 4)

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,24 +5,30 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\ProfilePasswordUpdateRequest;
 use App\Http\Requests\Admin\ProfileUpdateRequest;
+use App\Traits\FileUploadTrait;
 use Auth;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View as ViewView;
 
 class ProfileController extends Controller
 {
+    use FileUploadTrait;
+
     function index():View {
         return view('admin.profile.index');
     }
 
     function updateProfile(ProfileUpdateRequest $request):RedirectResponse {
-        dd($request->all());
 
         $user = Auth::user();
 
+        $imagePath = $this->uploadImage($request, 'avatar');
+
         $user->name = $request->name;
         $user->email = $request->email;
+        $user->avatar = isset($imagePath) ? $imagePath : $user->avatar;
         $user->save();
 
         toastr('Updated Successfully!', 'success');

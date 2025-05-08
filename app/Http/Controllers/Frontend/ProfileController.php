951b43b677057c063860030339cceae86b153f53
NikoKenara <nikokenara@gmail.com>
2025-05-08 13:19:54 +0700

    Frontend - Profile Update Feature for Frontend (Part - 2)

--- app/Http/Controllers/Frontend/ProfileController.php
+++ app/Http/Controllers/Frontend/ProfileController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Frontend\ProfileUpdateRequest;
+use Auth;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     function updateProfile(ProfileUpdateRequest $request) : RedirectResponse {
-        dd($request->all());
+        $user = Auth::user();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        toastr()->success('Profile Updated Successfully!');
+
+        return redirect()->back();
     }
 }

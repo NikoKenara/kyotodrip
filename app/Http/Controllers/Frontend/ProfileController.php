5b93ee3f50ec39b267b6bb3fc3623a73d6dce1bc
NikoKenara <nikokenara@gmail.com>
2025-05-12 23:45:35 +0700

    Frontend - Profile Password Update Feature

--- app/Http/Controllers/Frontend/ProfileController.php
+++ app/Http/Controllers/Frontend/ProfileController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\ProfilePasswordUpdateRequest;
 use App\Http\Requests\Frontend\ProfileUpdateRequest;
 use Auth;
+use Illuminate\Console\View\Components\Task;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
@@ -19,5 +21,15 @@ class ProfileController extends Controller
         toastr()->success('Profile Updated Successfully!');
 
         return redirect()->back();
+    }
+
+    function updatePassword(ProfilePasswordUpdateRequest $request) : RedirectResponse {
+        $user = Auth::user();
+        $user->password = bcrypt($request->password);
+        $user->save();
+        toastr()->success('Password Updated Successfully!');
+
+        return redirect()->back();
+
     }
 }

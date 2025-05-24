150b763654623296f3f703557bcda989b1aa3985
NikoKenara <nikokenara@gmail.com>
2025-05-24 18:18:46 +0700

    Why Choose Us - Working with Update Feature

--- app/Http/Controllers/Admin/WhyChooseUsController.php
+++ app/Http/Controllers/Admin/WhyChooseUsController.php
@@ -54,17 +54,24 @@ class WhyChooseUsController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id) : View
     {
-        //
+        $whyChooseUs = WhyChooseUs::findOrFail($id);
+        return view('admin.why-choose-us.edit', compact('whyChooseUs'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(WhyChooseUsCreateRequest $request, string $id) : RedirectResponse
     {
-        //
+        $whyChooseUs = WhyChooseUs::findOrFail($id);
+
+        $whyChooseUs->update($request->validated());
+
+        toastr()->success('Created Successfully');
+
+        return to_route('admin.why-choose-us.index');
     }
 
     public function updateTitle(Request $request) {

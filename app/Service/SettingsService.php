010f04c3960aa18e696c169948a132c5770ad208
NikoKenara <nikokenara@gmail.com>
2025-06-20 10:42:01 +0700

    General Settings - Accessing Settings Globally in Project (Part 1)

---
+++ app/Service/SettingsService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Setting;
+use Cache;
+
+class SettingsService {
+    function getSettings() {
+        return Cache rememberForever('settings', function(){
+            return Setting::pluck('value', 'key')->toArray();
+        });
+    }
+
+    function setGlobalSettings() {
+        $settings = $this->getSettings();
+        config()->set('settings', $settings);
+    }
+
+    function clearCachedSettings() {
+        Cache::forget('settings');
+    }
+}

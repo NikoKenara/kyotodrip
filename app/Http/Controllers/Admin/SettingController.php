7aa31332b9203346d3540280797c8a5cda3a8994
NikoKenara <nikokenara@gmail.com>
2025-06-20 11:09:29 +0700

    General Settings - Forgetting Setting Cache

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Setting;
+use App\Service\SettingsService;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -29,6 +30,9 @@ class SettingController extends Controller
                 ['value' => $value]
             );
         }
+
+        $settingService = app(SettingsService::class);
+        $settingService->clearCachedSettings();
 
         toastr()->success('Updated Successfully!');
 

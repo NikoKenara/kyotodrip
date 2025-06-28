9c6ff7702291ad2e2975f81f4973c5506183ca3c
NikoKenara <nikokenara@gmail.com>
2025-06-28 11:55:42 +0700

    Add to Cart - Working with Add to Cart Feature (Part - 3)

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -5,26 +5,33 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\ProductSize;
+use Gloudemans\Shoppingcart\Cart;
 use Illuminate\Http\Request;
+
 
 class CartController extends Controller
 {
     // add product in to cart
-    function addToCart(Request $request) {
+    function addToCart(Request $request)
+    {
         $product = Product::with(['productSizes', 'productOptions'])->findOrFail($request->product_id);
         $productSize = $product->productSizes->where('id', $request->product_size)->first();
         $productOptions = $product->productOptions->whereIn('id', $request->product_option);
 
         $options = [
             'product_size' => [
-                'id' => $productSize->id,
-                'name' => $productSize->name,
-                'price' => $productSize->price
+                'id' => $productSize?->id,
+                'name' => $productSize?->name,
+                'price' => $productSize?->price
             ],
-            'product_options' => []
+            'product_options' => [],
+            'product-info' => [
+                'image' => $product->thumb_image,
+                'slug' => $product->slug
+            ]
         ];
 
-        foreach($productOptions as $option) {
+        foreach ($productOptions as $option) {
             $options['product_options'][] = [
                 'id' => $option->id,
                 'name' => $option->name,
@@ -32,6 +39,15 @@ class CartController extends Controller
             ];
         }
 
-        dd($options);
+        Cart::add([
+            'id' => $product->id,
+            'name' => $product->name,
+            'qty' => $request->quantity,
+            'price' => $product->offer_price,
+            'weight' => 0,
+            'options' =>$options
+        ]);
+
+        return response(['status' => 'success', 'message' => 'Product added into cart'], 200);
     }
 }

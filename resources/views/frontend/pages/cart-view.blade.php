87947b9b5bd7a15b84f92ccff646eef17d01535c
NikoKenara <nikokenara@gmail.com>
2025-06-30 23:37:30 +0700

    Cart Page - Working with Remove Product

--- resources/views/frontend/pages/cart-view.blade.php
+++ resources/views/frontend/pages/cart-view.blade.php
@@ -96,7 +96,7 @@
                                             </td>
 
                                             <td class="fp__pro_icon">
-                                                <a href="#"><i class="far fa-times"></i></a>
+                                                <a href="#" class="reomove_cart_product" data-id="{{ $product->rowId }}"><i class="far fa-times"></i></a>
                                             </td>
                                         </tr>
                                     @endforeach
@@ -179,6 +179,34 @@
                         if(callback && typeof callback === 'function'){
                             callback(response);
                         }
+                    },
+                    error: function(xhr, status, error) {
+                        let errorMessage = xhr.responseJSON.message;
+                        hideLoader();
+                        toastr.error(errorMessage);
+                    },
+                    complete: function() {
+                        hideLoader();
+                    }
+                })
+            }
+
+            $('.reomove_cart_product').on('click', function(e){
+                e.preventDefault();
+                let rowId = $(this).data('id');
+                removeCartProduct(rowId);
+                $(this).closest('tr').remove();
+            })
+
+            function removeCartProduct(rowId){
+                $.ajax({
+                    method: 'get',
+                    url: '{{ route("cart-product-remove", ":rowId") }}'. replace(":rowId", rowId),
+                    beforeSend: function() {
+                        showLoader();
+                    },
+                    success: function(response) {
+                        //
                     },
                     error: function(xhr, status, error) {
                         let errorMessage = xhr.responseJSON.message;

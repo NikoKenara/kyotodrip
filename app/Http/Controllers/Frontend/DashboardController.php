7bf4321056a83cb99104ef0e5886c4ace9e725f4
NikoKenara <nikokenara@gmail.com>
2025-07-05 20:09:07 +0700

    Customer Address - Working with Create Feature (Part - 2)

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\DeliveryArea;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     function index() : View {
-        return view('frontend.dashboard.index');
+        $deliveryAreas = DeliveryArea::where('status', 1)->get();
+        return view('frontend.dashboard.index', compact('deliveryAreas'));
+    }
+
+    function createAddress(Request $request) {
+        dd($request->all());
     }
 }

1e2bb90a17ffa219a16dbc3aa60baf59d7bb4cff
NikoKenara <nikokenara@gmail.com>
2025-07-05 12:21:22 +0700

    Delivery Areas - Working with Create Feature (Part - 1)

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -140,7 +140,7 @@
 
             <li class="dropdown">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
-                        class="fas fa-columns"></i> <span>Manage Restaurent</span></a>
+                        class="fas fa-columns"></i> <span>Manage Ecommerce</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="#">Coupon (not available)</a></li>
                     <li><a class="nav-link" href="{{ route('admin.delivery-area.index') }}">Delivery Area</a></li>

caf87bcc5ef097b2b34617f343eb742d08c65dac
NikoKenara <nikokenara@gmail.com>
2025-07-28 12:14:16 +0700

    8_PayPal Integration - Working with PayPal Integration (Part - 2)

--- app/Http/Controllers/Frontend/PaymentController.php
+++ app/Http/Controllers/Frontend/PaymentController.php
@@ -10,8 +10,9 @@ use Illuminate\Validation\ValidationException;
 
 class PaymentController extends Controller
 {
-    function index() : View {
-        if(!session()->has('delivery_fee') || !session()->has('address')){
+    function index(): View
+    {
+        if (!session()->has('delivery_fee') || !session()->has('address')) {
             throw ValidationException::withMessages(['Something went wrong']);
         }
         $subtotal = cartTotal();
@@ -26,13 +27,14 @@ class PaymentController extends Controller
         ));
     }
 
-    function makePayment(Request $request, OrderService $orderService) {
+    function makePayment(Request $request, OrderService $orderService)
+    {
         $request->validate([
             'payment_gateway' => ['required', 'string', 'in:paypal']
         ]);
 
         // create order
-        if($orderService->createOrder()){
+        if ($orderService->createOrder()) {
             // redirect user to the payment host
             // return true;
             switch ($request->payment_gateway) {
@@ -49,13 +51,40 @@ class PaymentController extends Controller
 
     // paypal payment
 
-    function payWithPaypal() {
-        return 'Payment processing';
+    function setPaypalConfig(): array
+    {
+        $config = [
+            'mode'    => config('gatewaySettings.paypal_account_mode'), // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
+            'sandbox' => [
+                'client_id'         => config('gatewaySettings.paypal_api_key'),
+                'client_secret'     => config('gatewaySettings.paypal_secret_key'),
+                'app_id'            => 'APP-80W284485P519543T',
+            ],
+            'live' => [
+                'client_id'         => config('gatewaySettings.paypal_api_key'),
+                'client_secret'     => config('gatewaySettings.paypal_secret_key'),
+                'app_id'            => env('PAYPAL_LIVE_APP_ID', ''),
+            ],
+
+            'payment_action' => 'Sale', // Can only be 'Sale', 'Authorization' or 'Order'
+            'currency'       => config('gatewaySettings.paypal_currency'),
+            'notify_url'     => env('PAYPAL_NOTIFY_URL', ''), // Change this accordingly for your application.
+            'locale'         => 'en_US', // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
+            'validate_ssl'   => true, // Validate SSL when creating api client.
+        ];
+
+        return $config;
     }
-    function paypalSuccess() {
+    function PayWithPaypal()
+    {
         //
     }
-    function paypalCancel() {
+    function paypalSuccess()
+    {
+        //
+    }
+    function paypalCancel()
+    {
         //
     }
 }
